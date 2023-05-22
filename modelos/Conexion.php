0cab50b4ff1028d8b185b836944c06acaf36054c
marino5710 <brianmarin4@gmail.com>
2023-05-22 08:23:56 -0600

    se creo el try y se copio el pdo del repositorio practica 7

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -4,5 +4,6 @@ abstract class Conexion{
     public static $conexion = null; 
     private static function conectar(){
         try{
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
     }
 }

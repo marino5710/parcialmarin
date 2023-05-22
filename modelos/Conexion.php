7a80fba00a717f55ead8ec98661c72119569ef92
marino5710 <brianmarin4@gmail.com>
2023-05-22 08:24:59 -0600

    se creo el set attibute

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -5,5 +5,7 @@ abstract class Conexion{
     private static function conectar(){
         try{
             self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     }
 }

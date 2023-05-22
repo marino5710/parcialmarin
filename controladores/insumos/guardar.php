b1af92534060f535a1abc5bd8b595c40537d45d2
marino5710 <brianmarin4@gmail.com>
2023-05-22 08:58:53 -0600

    se agrego link de boostrap 5

--- controladores/insumos/guardar.php
+++ controladores/insumos/guardar.php
@@ -3,13 +3,12 @@
 require '../../modelos/Insumo.php'; 
 
 try {
-    $insumo = new Insumo($_POST);
+    $insumo = new Insumos($_POST);
     $resultado = $insumo->guardar();
     $error= "No se guardo correctamente"; 
-
 } catch (PDOException $e)) {
     $error = $e->getMessage();
-}catch (Exception $e2){
+}catch (Exception $e2){ 
     $error = $e2->getMessage(); 
 }
 
--- vistas/insumos/index.php
+++ vistas/insumos/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>REGISTRO DE INSUMOS</title>
 </head>
 <body>
@@ -21,7 +22,7 @@
                 <div class="row mb-3">
                     <div class="col">
                         <label for="insum_cantidad">Cantidad del insumo</label>
-                        <input type="text" name="insum_cantidad" id="insum_cantidad" class="form-control" placeholder="INGRESE UNA CANTIDAD, EJ; 5">
+                        <input type="number" name="insum_cantidad" id="insum_cantidad" class="form-control" placeholder="INGRESE UNA CANTIDAD, EJ; 5">
                     </div>
                 </div>
                 <div class="row mb-3">
@@ -39,7 +40,7 @@
                 <div class="row mb-3">
                     <div class="col">
                         <label for="insum_precio">Precio del insumo</label>
-                        <input type="text" name="insum_precio" id="insum_precio" class="form-control" placeholder="INGRESE UN PRECIO, EJ; 5">
+                        <input type="float" name="insum_precio" id="insum_precio" class="form-control" placeholder="INGRESE UN PRECIO, EJ; 5">
                     </div>
                 </div>
                 <div class="row mb-3">

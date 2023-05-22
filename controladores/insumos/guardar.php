9aafe8d9475ef22bab34b6f1395a2d7c4bafb570
marino5710 <brianmarin4@gmail.com>
2023-05-22 08:53:13 -0600

    se termino de crear el catch

--- controladores/insumos/guardar.php
+++ controladores/insumos/guardar.php
@@ -2,3 +2,14 @@
 
 require '../../modelos/Insumo.php'; 
 
+try {
+    $insumo = new Insumo($_POST);
+    $resultado = $insumo->guardar();
+    $error= "No se guardo correctamente"; 
+
+} catch (PDOException $e)) {
+    $error = $e->getMessage();
+}catch (Exception $e2){
+    $error = $e2->getMessage(); 
+}
+

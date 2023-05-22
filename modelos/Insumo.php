1a492f56e60fa50581e996ef46266a5f8a57cf03
marino5710 <brianmarin4@gmail.com>
2023-05-22 08:37:19 -0600

    se creo la clase insumos

---
+++ modelos/Insumo.php
@@ -0,0 +1,11 @@
+<?php
+
+class Insumos extends Conexion {
+    public $insum_id; 
+    public $insum_nombre;
+    public $insum_cantidad;
+    public $insum_marca;
+    public $insum_proveedor;
+    public $insum_precio;
+
+}
